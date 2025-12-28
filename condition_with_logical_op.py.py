67af18f78688dba1a102fdee1a019f648bb68d0b
Ayushmittal99 <mittalayush520@gmail.com>
2025-12-28 14:38:51 +0530

    conditions in python
    how can we apply different types of conditions in python

---
+++ condition_with_logical_op.py.py
@@ -0,0 +1,12 @@
+ch = 't'
+
+if (ch in "aeiou"):
+    print("Vowel")
+else:
+    print("Not Vowel")
+# if (ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u'):
+#     print("not consonant")
+
+# else:
+#     print("consonant")
+
---
+++ conditional.py
@@ -0,0 +1,26 @@
+#           if syntax 
+# if (condition):
+#     code
+
+#           if else syntax 
+# if (condition):
+#     code
+
+# else:
+#     code
+
+
+# example
+
+age = 19
+if (age>=18):
+    print("u can vote")
+
+#if else synatx 
+
+age = 19
+if (age>=18):
+    print("u can vote")
+
+else:
+    print("u can't vote ")
