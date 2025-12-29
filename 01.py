164205df7d2f4251f0dffdedcfa1c58615b7fc9a
Ayushmittal99 <mittalayush520@gmail.com>
2025-12-29 18:20:46 +0530

    dictionary
    how the dictionary help's to sort the data in the kry values formate

---
+++ 01.py
@@ -0,0 +1,44 @@
+# # BIGO    O
+#      # aman and isha  are KEYS
+# name = {"aman" : 3579245 , "isha" : 3472379}
+# # print(name ["aman"])
+
+
+# #                  UPDATE THE VALUE NOT HE KEY   
+# name ["aman"] = 12345
+# # print(name)
+
+
+# #                  INSERT 
+# name["kajal"]=532632
+# # print(name)
+
+
+# #                   DELETE
+# a = name.pop("aman")
+# print(name)
+# print("a ", a) # store the pop vlaue 
+
+
+#                     FOR UPDATE OR ADDITION TO THE EXCISCTING KEY 
+# mydictionary = {"b":2000 , "a":3000}
+# mydictionary["a"] = mydictionary["a"]+5000
+# print(mydictionary)
+
+
+# a = {"total" : 0}
+
+# for i in range (1 , 88):
+#     if(i%2 == 0):
+#         a["total"] = a["total"] + 1
+
+# print(a["total"])
+
+
+#   CHECK THE KEY WHEATER IT IS IN DICtionary OR NOT 
+# b = {"a" : 234 , "d" : 3456}
+
+# print ("a" in b)
+
+
+# ipynb
